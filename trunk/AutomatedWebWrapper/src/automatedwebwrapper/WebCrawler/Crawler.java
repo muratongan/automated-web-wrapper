7b5c6c0a59a43f0a8604e9417b126ebca6b16114
mh.nassabi <mh.nassabi@2a5d6953-edd6-f0bb-ff3a-687459723ead>
2010-12-30 21:34:01 +0000

    first commit

---
+++ trunk/AutomatedWebWrapper/src/automatedwebwrapper/WebCrawler/Crawler.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package automatedwebwrapper.WebCrawler;
+
+import java.net.URL;
+import automatedwebwrapper.WebCrawler.Interfaces.InterfaceCrawlQueue;
+import automatedwebwrapper.WebCrawler.Interfaces.InterfaceThreadMsgReceiver;
+
+/**
+ * M. H. Nassabi
+ * @author s098828
+ */
+public class Crawler implements InterfaceThreadMsgReceiver {
+
+    public Crawler(URL crawlURL, InterfaceCrawlQueue crawlQueue, int maxLevel, int maxThreads)
+            throws InstantiationException, IllegalAccessException{
+
+        ThreadManager threadManager = new ThreadManager(crawlURL, 0, maxLevel, maxThreads, crawlQueue, this, CrawlThread.class);
+
+        }
+    
+    
+    public void msgReceived(Object theMessage, int threadId) {
+        System.out.println("Thread [" + threadId + "] " + "Started Processing " + theMessage.toString());
+    }
+
+    public void threadFinishedProcessing(int threadId) {
+        System.out.println("Thread with threadId: [" + threadId + "] finished processesing");
+    }
+
+    public void allThreadsFinishedProcessing() {
+        System.out.println("\n*********THE CRAWLER HAS FINISHED CRAWLING*********");
+    }
+
+
+
+
+    
+
+}
