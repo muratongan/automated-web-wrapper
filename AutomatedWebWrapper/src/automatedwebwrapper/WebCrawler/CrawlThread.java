13606b26b27dae1d359077b466e95bb0fdadeba6
mh.nassabi <mh.nassabi@2a5d6953-edd6-f0bb-ff3a-687459723ead>
2011-01-01 14:01:23 +0000

    first commit

--- AutomatedWebWrapper/src/automatedwebwrapper/WebCrawler/CrawlThread.java
+++ AutomatedWebWrapper/src/automatedwebwrapper/WebCrawler/CrawlThread.java
@@ -1,50 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package automatedwebwrapper.WebCrawler;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Vector;
-import automatedwebwrapper.WebCrawler.AbstractClasses.BaseCrawlThread;
-import automatedwebwrapper.WebCrawler.UtilityClasses.URLExtractor;
 
 /**
  * M. H. Nassabi
  * @author s098828
  */
-public class CrawlThread extends BaseCrawlThread {
-
-    @Override
-    public void process(Object o) {
-		try {
-			URL pageURL = (URL) o;
-
-                        URL baseURL = threadManager.getBaseURL();
-
-                        String rawPage = URLExtractor.getURL(pageURL);
-                        String smallPage = rawPage.toLowerCase().replaceAll("\\s", " ");
-			
-			Vector links = URLExtractor.extractLinks(rawPage, smallPage);
-			
-			for (int n = 0; n < links.size(); n++) {
-				try {
-					// urls might be relative to current page
-					URL link = new URL(pageURL, (String) links.elementAt(n));
-
-                                        if (!link.toString().startsWith(baseURL.toString()))
-                                            continue;
-
-                                        // If layers are not used, write everything into same layer
-                                        if (threadManager.getMaxCrawlLevel() == -1)
-						queue.push(link, level);
-					else
-						queue.push(link, level + 1);
-				} catch (MalformedURLException e) {
-
-                                }
-				
-			}
-		} catch (Exception e) {}
-    }
-    
+public class CrawlThread {
 
 }
