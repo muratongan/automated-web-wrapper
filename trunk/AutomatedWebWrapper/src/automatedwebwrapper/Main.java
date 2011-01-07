4be5365923860d1e73604d953dfbd3ac3caf5b61
onganmurat <onganmurat@2a5d6953-edd6-f0bb-ff3a-687459723ead>
2011-01-07 18:27:28 +0000

    integrate classifier with GUI

--- trunk/AutomatedWebWrapper/src/automatedwebwrapper/Main.java
+++ trunk/AutomatedWebWrapper/src/automatedwebwrapper/Main.java
@@ -5,6 +5,7 @@
 
 package automatedwebwrapper;
 
+import automatedwebwrapper.tree.TreeBuilder;
 import java.util.Set;
 
 
@@ -20,8 +21,14 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         automatedwebwrapper.WebCrawler.Main exMain = new automatedwebwrapper.WebCrawler.Main();
-        Set processedURLs = exMain.startCrawl(); //This is the set of all Processed URLS crawled by the crawler
-        
+        Set<String> processedURLs = exMain.startCrawl(); //This is the set of all Processed URLS crawled by the crawler
+        System.out.println("number of urls: " + processedURLs.size());
+        for (String item : processedURLs) {
+            System.out.println(item);
+            TreeBuilder treeBuilder = new TreeBuilder(item);
+            System.out.println();
+            System.out.println(treeBuilder);
+            break;
+        }
     }
-
 }
