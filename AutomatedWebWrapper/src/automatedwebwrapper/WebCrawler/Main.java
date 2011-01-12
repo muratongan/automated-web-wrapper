606e14f4a019f049942d546ec2e69d9ce2f1bd82
mh.nassabi <mh.nassabi@2a5d6953-edd6-f0bb-ff3a-687459723ead>
2011-01-12 14:24:27 +0000

    NewMainWebcrawler

--- AutomatedWebWrapper/src/automatedwebwrapper/WebCrawler/Main.java
+++ AutomatedWebWrapper/src/automatedwebwrapper/WebCrawler/Main.java
@@ -37,7 +37,12 @@ public class Main {
                         mainThread.start();
                         URL crawlURL = new URL(mainURL);
                         int maxCrawlLevel = 2;
-			int maxNumberThreads = 5;
+			int maxNumberThreads = 7;
+
+                        System.out.println("Crawler is initiated with => " + maxNumberThreads + " threads.");
+                        System.out.println("Crawler has maximum crawl level of => " + maxCrawlLevel + ".");
+                        System.out.println("Maximum number of URLs to be crawled are => " + maxNumberOfURLs + " URLs");
+                        System.out.println("*************Crawler Started Crawling*******************");
 
                         CrawlQueue crawlQueue = new CrawlQueue();
 			crawlQueue.setFilenamePrefix("");
